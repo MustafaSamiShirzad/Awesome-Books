afde203f3b7abf2cf2d669670a57288bd23224e3
MustafaSamiShirzad <mustafasami209@gmail.com>
2023-07-04 03:01:41 +0500

    Configured Linters

--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,15 @@
-const addButton = document.querySelector(".add_button");
-const bookTitle = document.querySelector(".book_title");
-const authorName = document.querySelector(".author_name");
-const article = document.querySelector(".book_details");
+const addButton = document.querySelector('.add_button');
+const bookTitle = document.querySelector('.book_title');
+const authorName = document.querySelector('.author_name');
+const article = document.querySelector('.book_details');
 
-
+let allBooks = [];
 if (localStorage.getItem('allBooks') != null) {
-  allBooks = JSON.parse(localStorage.getItem('allBooks'))
-}else{
-   allBooks = [{
-    title : "Mistborn",
-    author : "Brandon Sanderson"
+  allBooks = JSON.parse(localStorage.getItem('allBooks'));
+} else {
+  allBooks = [{
+    title: 'Mistborn',
+    author: 'Brandon Sanderson',
   }];
 }
 
@@ -18,23 +18,22 @@ function Book(title, author) {
   this.author = author;
 }
 
-function updateLocalStorage (){
+function updateLocalStorage() {
   localStorage.setItem('allBooks', JSON.stringify(allBooks));
 }
 
-function removeBook (bookId, obj) {
-  let bookToRemove = document.querySelector(`#${bookId}`);
+function removeBook(bookId, obj) {
+  const bookToRemove = document.querySelector(`#${bookId}`);
   article.removeChild(bookToRemove);
-  allBooks.splice(allBooks.indexOf(obj),1);
-  console.log(allBooks);
+  allBooks.splice(allBooks.indexOf(obj), 1);
   updateLocalStorage();
 }
 
 function displayBookDetails(obj) {
-  let bookContainer = document.createElement("div");
-  let bookId = "a" + Math.floor(Math.random()*100000);
+  const bookContainer = document.createElement('div');
+  const bookId = `a${Math.floor(Math.random() * 100000)}`;
   bookContainer.id = bookId;
-  let buttonId = "a" + Math.floor(Math.random()*100000);
+  const buttonId = `a${Math.floor(Math.random() * 100000)}`;
   bookContainer.innerHTML = `
     <p class="book_title">${obj.title}</p>
     <p class="author_name">${obj.author}</p>
@@ -43,24 +42,18 @@ function displayBookDetails(obj) {
     <hr>
     `;
   article.append(bookContainer);
-  let removeBtn = document.querySelector(`#${buttonId}`);
+  const removeBtn = document.querySelector(`#${buttonId}`);
   removeBtn.addEventListener('click', (evt) => { evt.stopPropagation(); removeBook(bookId, obj); });
-  
 }
 
 function addBook() {
   const newBook = new Book(bookTitle.value, authorName.value);
   allBooks.push(newBook);
   displayBookDetails(newBook);
-  bookTitle.value = "";
-  authorName.value = "";
-  updateLocalStorage()
+  bookTitle.value = '';
+  authorName.value = '';
+  updateLocalStorage();
 }
 
-addButton.addEventListener("click", addBook);
+addButton.addEventListener('click', addBook);
 window.onload = allBooks.forEach(displayBookDetails);
-
-
-
-
-
