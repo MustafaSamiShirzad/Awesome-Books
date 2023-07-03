0c5e19540217ede396b2736d296cee45a00303d5
IsmaelMastronardi <128931752+IsmaelMastronardi@users.noreply.github.com>
2023-07-03 17:31:30 -0300

    add remove button functionality

--- assets/script.js
+++ assets/script.js
@@ -3,28 +3,47 @@ const bookTitle = document.querySelector(".book_title");
 const authorName = document.querySelector(".author_name");
 const article = document.querySelector(".book_details");
 
-const allBooks = [];
+const allBooks = [{
+  title : "Mistborn",
+  author : "Brandon Sanderson"
+}];
 
 function Book(title, author) {
   this.title = title;
   this.author = author;
 }
 
+function removeBook (bookId, obj) {
+  let bookToRemove = document.querySelector(`#${bookId}`);
+  article.removeChild(bookToRemove);
+  allBooks.splice(allBooks.indexOf(obj),1);
+  console.log(allBooks)
+}
+
 function displayBookDetails(obj) {
   let bookContainer = document.createElement("div");
+  let bookId = `a${Date.now()}`;
+  bookContainer.id = bookId;
+  let buttonId = `a${Date.now()}`;
   bookContainer.innerHTML = `
     <p class="book_title">${obj.title}</p>
     <p class="author_name">${obj.author}</p>
-    <button class="remove_button">Remove</button>
+    <button class="remove_button"
+    id="${buttonId}">Remove</button>
     <hr>
     `;
   article.append(bookContainer);
+  let removeBtn = document.querySelector(`#${buttonId}`);
+  removeBtn.addEventListener('click', (evt) => { evt.stopPropagation(); removeBook(bookId, obj); });
+  
 }
 
 function addBook() {
   const newBook = new Book(bookTitle.value, authorName.value);
   allBooks.push(newBook);
   displayBookDetails(newBook);
+  bookTitle.value = "";
+  authorName.value = "";
 }
 
 addButton.addEventListener("click", addBook);
